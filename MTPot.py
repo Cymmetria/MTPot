6e49e7dab5c20ecc507b400a0a1f8d9202a580e9
Itamar Sher <itamar.sher@cymmetria.com>
2016-11-14 17:18:35 +0200

    * Added the source port to the ip logging * Added the -o --output flag * Removed redundent lien breaks in some logging messages

--- MTPot.py
+++ MTPot.py
@@ -14,8 +14,6 @@ BUSY_BOX = "/bin/busybox"
 MIRAI_SCANNER_COMMANDS = ["shell", "sh", "enable"]
 FINGERPRINTED_IPS = []
 
-logging.basicConfig(level=logging.INFO,\
-    format='%(asctime)s [%(name)s] %(levelname)s %(filename)s:%(lineno)s %(message)s')
 honey_logger = logging.getLogger("HoneyTelnet")
 syslogger = None
 config = None
@@ -33,23 +31,43 @@ class MyTelnetHandler(TelnetHandler):
     @command([BUSY_BOX])
     def handle_busybox(self, params):
         full_response = self.get_busybox_response(params)
-        honey_logger.debug("[%s] Responding: %s", self.client_address[0], full_response)
+        honey_logger.debug(
+            "[%s:%d] Responding: %s",
+            self.client_address[0],
+            self.client_address[1],
+            full_response.strip())
         self.writeresponse(full_response)
 
     def is_fingerprinted(self):
         if all([COMMANDS_EXECUTED[self.client_address[0]].count(cmd) > 0 for cmd in COMMANDS]):
-            honey_logger.info("%s: confirmed IP: %s", config.ddos_name, self.client_address[0])
+            honey_logger.info(
+                "%s: confirmed IP: [%s:%d]",
+                config.ddos_name,
+                self.client_address[0],
+                self.client_address[1])
             if syslogger:
-                syslogger.info("%s: confirmed IP: %s", config.ddos_name, self.client_address[0])
+                syslogger.info(
+                    "%s: confirmed IP: [%s:%d]",
+                    config.ddos_name,
+                    self.client_address[0],
+                    self.client_address[1])
             FINGERPRINTED_IPS.append(self.client_address[0])
             return True
         else:
             return False
 
     def store_command(self, cmd):
-        honey_logger.debug("%s executed: %s", self.client_address[0], cmd)
+        honey_logger.debug(
+            "[%s:%d] executed: %s",
+            self.client_address[0],
+            self.client_address[1],
+            cmd.strip())
         if syslogger:
-            syslogger.debug("%s executed: %s", self.client_address[0], cmd)
+            syslogger.debug(
+                "[%s:%d] executed: %s",
+                self.client_address[0],
+                self.client_address[1],
+                cmd.strip())
         if self.client_address[0] in FINGERPRINTED_IPS:
             return
         if not COMMANDS_EXECUTED.has_key(self.client_address[0]):
@@ -73,7 +91,12 @@ class MyTelnetHandler(TelnetHandler):
         return response
 
     def authCallback(self, username, password):
-        honey_logger.info("[%s] logon credentials used: user:%s pass:%s\n", self.client_address[0], username, password)
+        honey_logger.info(
+            "[%s:%d] logon credentials used: user:%s pass:%s",
+            self.client_address[0],
+            self.client_address[1],
+            username,
+            password)
 
     def writeerror(self, text):
         '''Called to write any error information (like a mistyped command).
@@ -85,11 +108,11 @@ class MyTelnetHandler(TelnetHandler):
 
     def session_start(self):
         '''Called after the user logs in.'''
-        honey_logger.debug("[%s] session started", self.client_address[0])
+        honey_logger.debug("[%s:%d] session started", self.client_address[0], self.client_address[1])
 
     def session_end(self):
         '''Called after the user logs off.'''
-        honey_logger.debug("[%s] session ended", self.client_address[0])
+        honey_logger.debug("[%s:%d] session ended", self.client_address[0], self.client_address[1])
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -103,6 +126,12 @@ def get_args():
         action='store_true',
         required=False,
         help='Increase MTPot verbosity')
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        required=False,
+        help='Output the results to this file')
     return parser.parse_args()
 
 
@@ -113,6 +142,15 @@ def main():
 
     args = get_args()
     config = HoneyConfig(args.config)
+    if args.output:
+        logging.basicConfig(
+            filename = args.output,
+            level=logging.INFO,
+            format='%(asctime)s [%(name)s] %(levelname)s %(filename)s:%(lineno)s %(message)s')
+    else:
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s [%(name)s] %(levelname)s %(filename)s:%(lineno)s %(message)s')
     if args.verbose:
         honey_logger.setLevel(logging.DEBUG)
     try:
